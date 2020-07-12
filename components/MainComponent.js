6e91d4d180ac760996cb9206dcd2bacf74e075b7
Madhav Gautam <madhavgautam2016@gmail.com>
2020-07-12 09:14:01 +0545

    navigation part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -3,9 +3,11 @@ import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
 import Dishdetail from "./DishdetailComponent";
 import { View, Platform } from "react-native";
-import { createStackNavigator } from "react-navigation";
-// import { Constants } from "expo";
+import { createStackNavigator, createDrawerNavigator } from "react-navigation";
 import Constants from "expo-constants";
+import { Icon } from "react-native-elements";
+
+import Home from "./HomeComponent";
 
 const MenuNavigator = createStackNavigator(
   {
@@ -23,6 +25,45 @@ const MenuNavigator = createStackNavigator(
         color: "#fff",
       },
     },
+  }
+);
+
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8",
+      },
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerTintColor: "#fff",
+    }),
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        title: "Home",
+        drawerLabel: "Home",
+      },
+    },
+    Menu: {
+      screen: MenuNavigator,
+      navigationOptions: {
+        title: "Menu",
+        drawerLabel: "Menu",
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#D1C4E9",
   }
 );
 
@@ -47,7 +88,7 @@ class Main extends Component {
           paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
         }}
       >
-        <MenuNavigator />
+        <MainNavigator />
       </View>
     );
   }
