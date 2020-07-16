55ed22829fdd496700c53c8d187f4a51596fe0f6
Madhav Gautam <madhavgautam2016@gmail.com>
2020-07-16 13:35:11 +0545

    gestures part2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -41,10 +41,19 @@ function RenderDish(props) {
     else return false;
   };
 
+  // handleViewRef = (ref) => (this.view = ref);
+
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: (e, gestureState) => {
       return true;
     },
+    // onPanResponderGrant: () => {
+    //   this.view
+    //     .rubberBand(1000)
+    //     .then((endState) =>
+    //       console.log(endState.finished ? "finished" : "cancelled")
+    //     );
+    // },
     onPanResponderEnd: (e, gestureState) => {
       console.log("pan responder end", gestureState);
       if (recognizeDrag(gestureState)) {
@@ -80,6 +89,7 @@ function RenderDish(props) {
         animation="fadeInDown"
         duration={2000}
         delay={1000}
+        // ref={this.handleViewRef}
         {...panResponder.panHandlers}
       >
         <Card featuredTitle={dish.name} image={{ uri: baseUrl + dish.image }}>
