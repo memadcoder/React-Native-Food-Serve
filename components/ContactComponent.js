aec9f7e1986f37b9f06ee94413db977859da9c82
Madhav Gautam <madhavgautam2016@gmail.com>
2020-07-17 14:53:38 +0545

    MailComposer Api expo to send mail

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from "react-native-elements";
 
 import * as Animatable from "react-native-animatable";
+
+// import { MailComposer } from "expo";
+import * as MailComposer from "expo-mail-composer";
 
 class ContactUs extends Component {
   static navigationOptions = {
     title: "ContactUs",
   };
+
+  sendMail() {
+    MailComposer.composeAsync({
+      recipients: ["confusion@food.net"],
+      subject: "Enquiry",
+      body: "To whom it may concern:",
+    });
+  }
 
   render() {
     return (
@@ -20,6 +31,14 @@ class ContactUs extends Component {
             <Text style={{ margin: 10 }}>Tel: +852 1234 5678</Text>
             <Text style={{ margin: 10 }}>Fax: +852 8765 4321</Text>
             <Text style={{ margin: 10 }}>Email:confusion@food.net</Text>
+            <Button
+              title="Send Email"
+              buttonStyle={{ backgroundColor: "#512DA8" }}
+              icon={
+                <Icon name="envelope-o" type="font-awesome" color="white" />
+              }
+              onPress={this.sendMail}
+            />
           </Card>
         </Animatable.View>
       </View>
